7e238220230c539adb49b74187bc495a6304cf13
Kuba Karpierz <mr.karpierz@gmail.com>
2022-04-30 10:42:56 -0400

    Clean: removed unused

--- src/public/struct/ClientId.ts
+++
@@ -1,9 +0,0 @@
-export class ClientId {
-  public lobbyId: string;
-  public playerId: string;
-
-  constructor(lobbyId = '', playerId = '') {
-    this.lobbyId = lobbyId;
-    this.playerId = playerId;
-  }
-}
